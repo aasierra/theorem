f7e054ba363f4e8892e559f5e7aa0b8118024f51
Anthony Sierra <aasierra@live.com>
2015-10-31 15:55:31 -0400

    Enabled usage of ES6

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Theorem JS
-A ES5 library that adds methods to the Math class for working with JS.
+A ES6 transpiled via Babel to ES5 library that adds methods to the Math class for working with JS.
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
 
 gulp.task('default', function () {
   var jsdoc = require('gulp-jsdoc');
   gulp.src("./src/*.js").pipe(jsdoc("./documentation"));
+  gulp.src("./src/*.js")
+  .pipe(sourcemaps.init())
+  .pipe(babel())
+  .pipe(concat("all.js"))
+  .pipe(sourcemaps.write("."))
+  .pipe(gulp.dest("dist"));
 });
